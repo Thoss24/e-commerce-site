c316a88f0ff4e43a8643dadbfd4cd1c235acbff4
Thoss24 <t.fogarty24@outlook.com>
2023-10-23 20:52:30 +0100

    adding modal

--- src/components/products/ProductDetailModal.tsx
+++ src/components/products/ProductDetailModal.tsx
@@ -1,9 +1,14 @@
-
+import classes from './ProductDetailModal.module.css';
 
 const ProductDetailModal = () => {
 
     return (
-        <div></div>
+        <>
+            <div className={classes.backdrop}/>
+            <div className={classes.modal}>
+                <h1>Test</h1>
+            </div>
+        </>
     )
 };
 
--- src/components/products/ProductItem.tsx
+++ src/components/products/ProductItem.tsx
@@ -1,16 +1,28 @@
 import classes from './ProductItem.module.css';
 import { useState } from 'react';
+import ProductDetailModal from './ProductDetailModal';
 
 const ProductItem: React.FC<{name: string, imageUrl: string, itemDescription: string, id: number}> = (props) => {
 
     const [isDisplaying, setIsDisplaying] = useState(false);
 
+    const modal = (
+        <ProductDetailModal />
+    );
+
+    const inspectItemHandler = () => {
+        setIsDisplaying(true)
+    };
+
     return (
-        <div className={classes.shell}>
+        <div className={isDisplaying ? classes['product-item-container'] : ''}>
+        {isDisplaying && modal}
+        <div className={classes.shell} onClick={inspectItemHandler}>
             <h2>{props.name}</h2>
             <h2>{props.id}</h2>
             <img src={props.imageUrl} alt={props.imageUrl}></img>
             <p>{props.name}</p>
+        </div>
         </div>
     )
 };
