d737c39b00ab3f096e94980632dbfe44350210f4
Thoss24 <t.fogarty24@outlook.com>
2023-12-02 15:39:32 +0000

    replacing use scroll with useInView on images

--- src/routes/about/AboutPage.tsx
+++ src/routes/about/AboutPage.tsx
@@ -1,5 +1,6 @@
 import classes from "./AboutPage.module.css";
 import { motion, useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
 import ImageSection from "./ImagesAboutPage";
 
 const AboutPage = () => {
@@ -7,10 +8,24 @@ const AboutPage = () => {
   // hero image
   const heroImageOpacity = useTransform(scrollY, [0, 1100, 1300, 1500, 1600], [1, 0.9, 0.8, 0.7, 0.4]);
 
+  const heroTextRef = useRef<HTMLHRElement>(null);
+  const aboutContainer = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    container: aboutContainer,
+    target: heroTextRef,
+    offset: ["end end", "end start"]
+  });
+
+  // const heroTextOpacity = useTransform(scrollYProgress, [0], [])
+
   return (
-    <div className={classes.about}>
+    <div ref={aboutContainer} className={classes.about}>
       <div className={classes['hero-image']}>
-        <motion.img style={{ opacity: heroImageOpacity }} src="https://firebasestorage.googleapis.com/v0/b/react-http-6cb96.appspot.com/o/stylish-man.jpg?alt=media&token=1a61891a-15be-460d-b74b-68ae8730e076" />
+        <motion.h2 ref={heroTextRef} style={{ opacity: scrollYProgress }} className={classes['hero-text']}>
+          hero text
+        </motion.h2>
+        <motion.img src="https://firebasestorage.googleapis.com/v0/b/react-http-6cb96.appspot.com/o/stylish-man.jpg?alt=media&token=1a61891a-15be-460d-b74b-68ae8730e076" />
       </div>
         <ImageSection />
     </div>
--- src/routes/about/ImagesAboutPage.tsx
+++ src/routes/about/ImagesAboutPage.tsx
@@ -1,49 +1,39 @@
 import { useQuery } from "@tanstack/react-query";
 import classes from "./AboutPage.module.css";
-import { motion, useTransform, useScroll } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { fetchImages } from "../../utility/http";
 import { useRef } from "react";
 
 const ImageSection = () => {
 
-  const imageSectionOne = useRef<HTMLDivElement>(null);
+  const firstImgRef = useRef<HTMLImageElement>(null);
+  const firstImgInView = useInView(firstImgRef, { once: true});
 
-  const imageSectionTwo = useRef<HTMLDivElement>(null);
+  const secondImgRef = useRef<HTMLImageElement>(null);
+  const secondImgInView = useInView(firstImgRef, { once: true });
 
-  const imageSectionThree = useRef<HTMLDivElement>(null);
+  const thirdImgRef = useRef<HTMLImageElement>(null);
+  const thirdImgInView = useInView(firstImgRef, { once: true });
 
-  const { scrollYProgress } = useScroll({
-    target: imageSectionOne,
-    offset: ["0 1", "1 1"] // where 0 is the bottom of viewport and 1 is top of viewport of target the animation starts and ends when the bottom of the viewport goes x% beyond end of target starting from top of target
-  });
-
-  const { scrollYProgress: scrollYSectionTwo } = useScroll({
-    target: imageSectionOne,
-    offset: ["0 1", "1.90 1"] 
-  });
-
-  const { scrollYProgress: scrollYSectionThree } = useScroll({
-    target: imageSectionTwo,
-    offset: ["0 1", "1.90 1"] 
-  });
-
-  const { data: images } = useQuery({ // Figure out why these urls aren't working!!
-    queryKey: ['images'],
-    queryFn: fetchImages
+  const { data: images } = useQuery({
+    queryKey: ["images"],
+    queryFn: fetchImages,
   });
 
   return (
-    <div className={classes['images-section']}>
-      <div ref={imageSectionOne} className={classes["about-page-section"]}>
-        <motion.img
-          style={{
-            scale: scrollYProgress,
-            opacity: scrollYProgress
-          }}
-          className={classes.image}
-          src="https://firebasestorage.googleapis.com/v0/b/react-http-6cb96.appspot.com/o/hero.jpg?alt=media&token=0ef28aaf-ce03-409c-a451-98dd0b0f0bfd"
-          alt=""
-        />
+    <div className={classes["images-section"]}>
+      <div className={classes["about-page-section"]}>
+        {images && (
+          <motion.img
+            ref={firstImgRef}
+            initial={{ opacity: firstImgInView ? 1 : 0, scale: firstImgInView ? 1 : 0.5 }}
+            animate={{ opacity: firstImgInView ? 1 : 0, scale: firstImgInView ? 1 : 0.5}}
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className={classes.image}
+            src={images && images.pink}
+            alt=""
+          />
+        )}
         <div className={classes["about-page-copy"]}>
           <h2>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis,
@@ -56,18 +46,15 @@ const ImageSection = () => {
           </p>
         </div>
       </div>
-      <div ref={imageSectionTwo} className={classes["about-page-section"]}>
-        { images &&
-      <motion.img
-        style={{
-          opacity: scrollYSectionTwo,
-          scale: scrollYSectionTwo
-        }}
-          className={classes.image}
-          src="https://firebasestorage.googleapis.com/v0/b/react-http-6cb96.appspot.com/o/clothing-model.jpg?alt=media&token=b7a61a82-9877-4f48-b1ce-8e7c31e8cb26"
-          alt="model"
-        />
-      }
+      <div className={classes["about-page-section"]}>
+        {images && (
+          <motion.img
+            ref={secondImgRef}
+            className={classes.image}
+            src={images && images.clothing_model}
+            alt="model"
+          />
+        )}
         <div className={classes["about-page-copy"]}>
           <h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
@@ -80,18 +67,15 @@ const ImageSection = () => {
           </p>
         </div>
       </div>
-      <div ref={imageSectionThree} className={classes["about-page-section"]}>
-        { images && 
-        <motion.img
-        style={{
-          opacity: scrollYSectionThree,
-          scale: scrollYSectionThree
-        }}
-          className={classes.image}
-          src="https://firebasestorage.googleapis.com/v0/b/react-http-6cb96.appspot.com/o/dresses.jpg?alt=media&token=4795ffd0-ee10-4f65-b2b9-a6527d535f97"
-          alt=""
-        />
-        }
+      <div className={classes["about-page-section"]}>
+        {images && (
+          <motion.img
+            ref={thirdImgRef}
+            className={classes.image}
+            src={images && images.dresses}
+            alt=""
+          />
+        )}
         <div className={classes["about-page-copy"]}>
           <h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
